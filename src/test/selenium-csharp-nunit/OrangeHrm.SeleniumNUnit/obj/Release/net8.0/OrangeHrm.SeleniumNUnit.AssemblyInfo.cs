348b81d22835a07b817a08b4810364bbc2ac1667
Kavia AI <production@kavia.ai>
2026-02-16 19:35:32 +0000

    Checkpoint 20 - cma6c2b152

--- src/test/selenium-csharp-nunit/OrangeHrm.SeleniumNUnit/obj/Release/net8.0/OrangeHrm.SeleniumNUnit.AssemblyInfo.cs
+++ src/test/selenium-csharp-nunit/OrangeHrm.SeleniumNUnit/obj/Release/net8.0/OrangeHrm.SeleniumNUnit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrangeHrm.SeleniumNUnit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e70232b2ee0df85c5f910eb9f5d32f52f7ee9377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d9b8f196b93c61de7a82fa494e5597d72e065a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrangeHrm.SeleniumNUnit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrangeHrm.SeleniumNUnit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ test-artifacts/bdd-dryrun-logs/java/java-cucumber-dryrun-summary.md
@@ -0,0 +1,3 @@
+# Java Cucumber dry-run summary (pending)
+
+This file will be overwritten by CI after Java dry-run succeeds.
